8ab6f2f936558249ef9cb016b66260425fe9e838
Bruno Cruz <Bruno.g.rosa@hotmail.com.br>
2025-04-12 21:47:54 -0300

    Install and configure NodeJS

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { config } from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
+
+config({
+  path: '.env'
+}); 
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ?? 3001;
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(port, () => {
+    console.log(`Server is running on  http://localhost:${port}`);
+  });
+}
+bootstrap();
