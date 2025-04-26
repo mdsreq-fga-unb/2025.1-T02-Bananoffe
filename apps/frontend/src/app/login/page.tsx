3596918b6bea933fa95499cb808f6436d0365768
Guilherme Zanella <guilhermeczanella@gmail.com>
2025-04-26 12:34:45 -0300

    att pagina de login react-hook

--- apps/frontend/src/app/login/page.tsx
+++ apps/frontend/src/app/login/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import {
   Box,
   Flex,
@@ -10,24 +10,25 @@ import {
   Input,
   Container,
   Button,
+  Center,
 } from "@chakra-ui/react";
 import { PasswordInput } from "@/components/ui/password-input";
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 import React from "react";
 
 interface FormValues {
-  username: string;
+  email: string;
   password: string;
 }
 
 function Login() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-      } = useForm<FormValues>()
-    
-      const onSubmit = handleSubmit((data: any) => console.log(data))
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  const onSubmit = handleSubmit((data: any) => console.log(data));
 
   return (
     <Box height="100vh">
@@ -42,12 +43,12 @@ function Login() {
             height="100%"
           />
         </Box>
-        <Stack flex={"0.5"} backgroundColor={" #F1DD2F;"}>
+        <Stack flex={"0.5"} backgroundColor={" #F1DD2F"}>
           <Stack
             gap={"5px"}
             alignItems={"center"}
             justifyItems="center"
-            padding={"30px"}
+            padding={"60px 30px 30px 30px"}
           >
             <Text textStyle={"3xl"} fontWeight={"semibold"} color={"black"}>
               Login
@@ -56,24 +57,62 @@ function Login() {
               Entre para continuar
             </Text>
           </Stack>
-          <Separator />
-          <Container padding={"0px 30px 30px 30px"}>
+          <Separator size="md" />
+          <Container padding={"30px 30px 30px 30px"} width={"80%"}>
             <form onSubmit={onSubmit}>
-              <Stack gap="4" align="flex-start" maxW="sm">
-                <Field.Root invalid={!!errors.username}>
-                  <Field.Label color={'black'}>Username</Field.Label>
-                  <Input {...register("username")} />
-                  <Field.ErrorText>{errors.username?.message}</Field.ErrorText>
-                </Field.Root>
+              <Center>
+                <Stack gap="4">
+                  <Field.Root invalid={!!errors.email}>
+                    <Field.Label htmlFor="email" color={"black"}>Email</Field.Label>
+                    <Input
+                      variant="subtle"
+                      bgColor=" #D9D9D9"
+                      color={"black"}
+                      size="lg"
+                      type="email"
+                      {...register("email", {
+                        required: "Email é obrigatório",
+                        pattern: {
+                          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]+$/i,
+                          message: "Email inválido",
+                        },
+                      })}
+                    />
+                    <Field.ErrorText>{errors.email?.message}</Field.ErrorText>
+                  </Field.Root>
 
-                <Field.Root invalid={!!errors.password}>
-                  <Field.Label color={'black'}>Password</Field.Label>
-                  <PasswordInput {...register("password")} />
-                  <Field.ErrorText>{errors.password?.message}</Field.ErrorText>
-                </Field.Root>
+                  <Field.Root invalid={!!errors.password}>
+                    <Field.Label color={"black"}>Senha</Field.Label>
+                    <PasswordInput
+                      variant="subtle"
+                      bgColor=" #D9D9D9"
+                      color={"black"}
+                      size="lg"
+                      {...register("password", {
+                        required: "Senha é obrigatória",
+                        minLength: {
+                          value: 5,
+                          message: "Senha deve ter pelo menos 5 caracteres",
+                        },
+                      })}
+                    />
+                    <Field.ErrorText>
+                      {errors.password?.message}
+                    </Field.ErrorText>
+                  </Field.Root>
 
-                <Button type="submit">Submit</Button>
-              </Stack>
+                  <Button
+                    type="submit"
+                    bgColor={"#895023"}
+                    color={"black"}
+                    size="md"
+                    width={"40%"}
+                    alignSelf={"center"}
+                  >
+                    Entrar
+                  </Button>
+                </Stack>
+              </Center>
             </form>
           </Container>
         </Stack>
