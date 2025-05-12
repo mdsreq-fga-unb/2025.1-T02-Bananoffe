4a694e4d8881a86264efa3c928fa88804f3fc7f9
guilhermezan42 <guilhermeczanella@gmail.com>
2025-05-12 14:14:48 -0300

    Att: estilização

--- apps/frontend/src/app/admin_cardapio/page.tsx
+++ apps/frontend/src/app/admin_cardapio/page.tsx
@@ -4,9 +4,12 @@ import {
   Image,
   Input,
   InputGroup,
+  Spinner,
   Stack,
   Table,
   useBreakpointValue,
+  Text,
+  Center,
 } from "@chakra-ui/react";
 
 import { useProducts } from "@/hooks/useProducts";
@@ -16,30 +19,8 @@ import { MdSearch } from "react-icons/md";
 function adminCardapio() {
   const { products, getProducts, isLoading } = useProducts();
   const [searchTerm, setSearchTerm] = useState("");
-  const items = [
-    {
-      id: 1,
-      nome: "Banoffee 1",
-      descricao: "descrição banoffee 1",
-      precoTortaP: 5,
-      precoTortaG: 5,
-      precoPedacoP: 10,
-      precoPedacoG: 20,
-      quantidade: 5,
-      imagem: null,
-    },
-    {
-      id: 2,
-      nome: "Banoffee 2",
-      descricao: "descrição banoffee 2",
-      precoTortaP: 5,
-      precoTortaG: 5,
-      precoPedacoP: 10,
-      precoPedacoG: 20,
-      quantidade: 5,
-      imagem: null,
-    },
-  ];
+
+  const isMobile = useBreakpointValue({ base: true, md: false });
   const imageSize = "50px";
 
   const filteredProducts = products.filter(
@@ -53,9 +34,9 @@ function adminCardapio() {
   }, []);
 
   return (
-    <Box className="pageContainer">
-      <Stack className="pageContent" gap="10">
-        <InputGroup endElement={<MdSearch />} w="30%" ml="25px">
+    <Box className="pageContainer" p={5} bg="gray.50" minH="100vh">
+      <Stack className="pageContent" gap="2">
+        <InputGroup endElement={<MdSearch />} w="30%">
           <Input
             placeholder="Pesquise"
             bgColor="#ededed"
@@ -64,57 +45,70 @@ function adminCardapio() {
             onChange={(e) => setSearchTerm(e.target.value)}
           />
         </InputGroup>
-        <Table.Root stickyHeader interactive variant="outline" color="white">
-          <Table.Header>
-            <Table.Row>
-              <Table.ColumnHeader>Produto</Table.ColumnHeader>
-              <Table.ColumnHeader>Descrição</Table.ColumnHeader>
-              <Table.ColumnHeader>Preço Torta P</Table.ColumnHeader>
-              <Table.ColumnHeader>Preço Torta G</Table.ColumnHeader>
-              <Table.ColumnHeader>Preço Pedaço P</Table.ColumnHeader>
-              <Table.ColumnHeader>Preço Pedaço G</Table.ColumnHeader>
-              <Table.ColumnHeader>Quantidade</Table.ColumnHeader>
-              <Table.ColumnHeader>Imagem</Table.ColumnHeader>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {filteredProducts.map((item) => (
-              <Table.Row key={item.id}>
-                <Table.Cell>{item.nome}</Table.Cell>
-                <Table.Cell>{item.descricao}</Table.Cell>
-                <Table.Cell>{item.precoPedacoP}</Table.Cell>
-                <Table.Cell>{item.precoPedacoG}</Table.Cell>
-                <Table.Cell>{item.precoTortaP}</Table.Cell>
-                <Table.Cell>{item.precoTortaG}</Table.Cell>
-                <Table.Cell>{item.quantidade}</Table.Cell>
-                <Table.Cell
-                  width={imageSize}
-                  height={imageSize}
-                  padding={1} // Espaçamento interno reduzido
-                >
-                  <Box
-                    width="100%"
-                    height="100%"
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="center"
-                    bg="gray.100" // Fundo cinza claro para visualização
-                    borderRadius="md" // Bordas arredondadas
-                    overflow="hidden" // Esconde qualquer overflow da imagem
-                  >
-                    <Image
-                      src={item.imagem}
-                      maxW="100%"
-                      maxH="100%"
-                      objectFit="contain"
-                      alt={item.nome}
-                    />
-                  </Box>
-                </Table.Cell>
+        {isLoading ? (
+          <Center mx="auto" my={20} borderWidth={"0px"}>
+            <Spinner size="xl" color="blue.500" />
+          </Center>
+        ) : (
+          <Table.Root
+            stickyHeader
+            variant="outline"
+            colorPalette="gray"
+            color="black"
+            borderRadius="sm"
+            overflowY="auto"
+          >
+            <Table.Header>
+              <Table.Row>
+                <Table.ColumnHeader>Produto</Table.ColumnHeader>
+                <Table.ColumnHeader>Descrição</Table.ColumnHeader>
+                <Table.ColumnHeader>Preço Torta P</Table.ColumnHeader>
+                <Table.ColumnHeader>Preço Torta G</Table.ColumnHeader>
+                <Table.ColumnHeader>Preço Pedaço P</Table.ColumnHeader>
+                <Table.ColumnHeader>Preço Pedaço G</Table.ColumnHeader>
+                <Table.ColumnHeader>Quantidade</Table.ColumnHeader>
+                <Table.ColumnHeader>Imagem</Table.ColumnHeader>
               </Table.Row>
-            ))}
-          </Table.Body>
-        </Table.Root>
+            </Table.Header>
+            <Table.Body>
+              {filteredProducts.map((item, index) => (
+                <Table.Row key={index}>
+                  <Table.Cell>{item.nome}</Table.Cell>
+                  <Table.Cell>{item.descricao}</Table.Cell>
+                  <Table.Cell>{item.precoPedacoP}</Table.Cell>
+                  <Table.Cell>{item.precoPedacoG}</Table.Cell>
+                  <Table.Cell>{item.precoTortaP}</Table.Cell>
+                  <Table.Cell>{item.precoTortaG}</Table.Cell>
+                  <Table.Cell>{item.quantidade}</Table.Cell>
+                  <Table.Cell
+                    width={imageSize}
+                    height={imageSize}
+                    padding={1} // Espaçamento interno reduzido
+                  >
+                    <Box
+                      width="100%"
+                      height="100%"
+                      display="flex"
+                      alignItems="center"
+                      justifyContent="center"
+                      bg="gray.100" // Fundo cinza claro para visualização
+                      borderRadius="md" // Bordas arredondadas
+                      overflow="hidden" // Esconde qualquer overflow da imagem
+                    >
+                      <Image
+                        src={item.imagem}
+                        maxW="100%"
+                        maxH="100%"
+                        objectFit="contain"
+                        alt={item.nome}
+                      />
+                    </Box>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table.Root>
+        )}
       </Stack>
     </Box>
   );
